036e1d0aef0d552e7e23f27d338c79da5dccee18
Thomasklinkenbergmusic <thomasklinkenbergmusic@gmail.com>
2016-09-14 16:32:18 +0200

    Modified sox_script.sh inside sox_try1/. It can produce some sound now :)

--- sox_try1/sox_script.sh
+++ sox_try1/sox_script.sh
@@ -11,4 +11,17 @@ SETOFF=$(((RANDOM%($LENGHT - 5))+1))
 
 # Pakt nu elke keer random 5 seconde uit het nummer
 
-sox --norm=-6 $INFILE $OUTFILE trim $SETOFF 5
+sox --norm=-6 $INFILE rm1.wav trim $SETOFF 5
+sox --norm=-6 rm1.wav rm2.wav reverse
+sox --norm=-6 rm2.wav $OUTFILE reverse
+
+rm -rf rm1.wav
+rm -rf rm2.wav
+
+# Een beetje omslachtig, maar het systeem maakt nu rm1.wav,
+# bewerkt die en maakt weer een nieuwe aan. Eigenlijk wil je dat hij de hele
+# tijd zichzelf bewerkt. Alleen werkt het volgende niet:
+
+# sox --norm=-6 rm1.wav rm1.wav reverse reverb
+
+# Dan geeft hij errors
