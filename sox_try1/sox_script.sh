c8bb3e71b61daf2ebf85ff29dab63fa5d8dce3f0
Thomasklinkenbergmusic <thomasklinkenbergmusic@gmail.com>
2016-09-14 16:19:00 +0200

    Uploaded sox_script.sh inside sox_try1/ dir.

--- sox_try1/sox_script.sh
+++ sox_try1/sox_script.sh
@@ -5,5 +5,10 @@ echo "Executing $0"
 INFILE=$1
 OUTFILE=$2
 MPLAYONOFF=$3
+LENGHTFLOAT=$(soxi -D $INFILE)
+LENGHT=${LENGHTFLOAT%.*}
+SETOFF=$(((RANDOM%($LENGHT - 5))+1))
 
-sox --norm=-6 
+# Pakt nu elke keer random 5 seconde uit het nummer
+
+sox --norm=-6 $INFILE $OUTFILE trim $SETOFF 5
