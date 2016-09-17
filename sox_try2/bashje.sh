18587b958516bbe693705c5d06c31a4db2ddfec0
Thomasklinkenbergmusic <thomasklinkenbergmusic@gmail.com>
2016-09-17 14:14:13 +0200

    Edit to bashje.sh in sox_try2/ dir. Check comments in bash.sh for more information.

--- sox_try2/bashje.sh
+++ sox_try2/bashje.sh
@@ -13,21 +13,31 @@ LENGHT=${LENGHTFLOAT%.*}
 SETOFF1=$(((RANDOM%($LENGHT - 5))+1))
 SETOFF2=$(((RANDOM%($LENGHT - 3))+1))
 SETOFF3=$(((RANDOM%($LENGHT - 2))+1))
+SETOFF4=$(((RANDOM%($LENGHT - 6))+1))
 PAN1=$(((RANDOM%(18))+1))
 PAN2=$(((RANDOM%(18))+1))
 
 # echo "Setoff _file1 = $SETOFF1"
 # echo "Setoff _file2 = $SETOFF2"
 # echo "Setoff _file3 = $SETOFF3"
+# echo "Setoff _file4 = $SETOFF4"
 # echo "Pan1 = $PAN1"
 # echo "Pan2 = $PAN2"
 # echo
 
-sox --norm=-6 $INFILE ${OUTFILE}_file1.wav trim $SETOFF1 5 fade 0.1 0 0.1
+# De comments hierboven kunnen alle waardes printen
 
-sox --norm=-6 $INFILE ${OUTFILE}_file2.wav trim $SETOFF2 3 lowpass 500 reverb 25 remix 1p-$PAN1 2p-$PAN2 2 fade 0.1 0 0.1
+sox $INFILE ${OUTFILE}_file1.wav trim $SETOFF1 5 norm -6 fade 0.1 0 0.1
+# Maakt een bestand van 5 seconde puur geluid
 
-sox $INFILE ${OUTFILE}_file3.wav trim $SETOFF3 2 overdrive 20 20 overdrive 20 20 overdrive 20 20 gain -12 fade 0.1 0 0.1
+sox $INFILE ${OUTFILE}_file2.wav trim $SETOFF2 3 lowpass 500 reverb 25 remix 1p-$PAN1 2p-$PAN2 2 norm -6 fade 0.1 0 0.1
+# Maakt een bestand van 3 seconde met een lowpass, reverb en een (random) panning effect
+
+sox $INFILE ${OUTFILE}_file3.wav trim $SETOFF3 2 overdrive 20 20 overdrive 20 20 overdrive 20 20 norm -18 fade 0.1 0 0.1
+# Maakt een bestand van 2 seconde met keiharde overdrive
+
+sox $INFILE ${OUTFILE}_file4.wav trim $SETOFF4 6 reverse fade 0.1 0 0.1 norm -6 fade 0.1 0 0.1
+# Maakt een bestand van 6 seconde die volledig gereversed is
 
 echo "Done!"
 echo "-----"
