a152462ff3656302a4b82b1d0f372baa4947cd54
Thomasklinkenbergmusic <thomasklinkenbergmusic@gmail.com>
2016-09-21 12:47:00 +0200

    Modified sox_try3/bashscript.sh - Improved version

--- sox_try3/bashscript.sh
+++ sox_try3/bashscript.sh
@@ -8,20 +8,35 @@ echo "---------------"
 echo
 #-------------------------------------------------------------------------------
 
+# Wat er nog beter moet:
+#   - Dat ie ten eerste spaties verwijderd uit bestandsnamen!
+#   - De recursieve mix functie
+#   - Dat is het voor nu
+
 INDIR=$1
 
-  if [ -d $1 ]
-    then
-      echo "Input directory is $1"
-      echo
-    else
-      echo "Error with input directory."
-      echo "Can't find director $1"
-      echo
-      echo "... Exiting program"
-      echo
-      exit
-    fi
+if [ $# -lt 1 ];
+  then
+    echo "Error with input directory"
+    echo "No argument!"
+    echo
+    echo "... Exiting program"
+    echo
+    exit
+fi
+
+if [ -d $1 ]
+  then
+    echo "Input directory is $1"
+    echo
+  else
+    echo "Error with input directory."
+    echo "Can't find director $1"
+    echo
+    echo "... Exiting program"
+    echo
+    exit
+  fi
 
 if [ -d output ]
   then
@@ -39,7 +54,6 @@ echo "Processing files..."
 echo
 
 cd $INDIR
-I=0;
 
 for f in *.mp3;
   do
@@ -51,11 +65,11 @@ for f in *.mp3;
     SETOFF4=$(((RANDOM%($LENGHT - 6))+1))
     PAN1=$(((RANDOM%(18))+1))
     PAN2=$(((RANDOM%(18))+1))
-    sox $f ../output/${I}_trim.wav trim $SETOFF1 5 norm -6 fade 0.1 0 0.1
-    sox $f ../output/${I}_otherroom.wav trim $SETOFF2 3 lowpass 500 reverb 25 remix 1p-$PAN1 2p-$PAN2 2 norm -6 fade 0.1 0 0.1
-    sox $f ../output/${I}_destruction.wav trim $SETOFF3 2 overdrive 20 20 overdrive 20 20 overdrive 20 20 norm -18 fade 0.1 0 0.1
-    sox $f ../output/${I}_reverse.wav trim $SETOFF4 6 reverse fade 0.1 0 0.1 norm -6 fade 0.1 0 0.1
-	  I=$((I+1));
+    file=$(basename $f .mp3)
+    sox $f ../output/${file}_trim.wav trim $SETOFF1 5 norm -6 fade 0.1 0 0.1
+    sox $f ../output/${file}_otherroom.wav trim $SETOFF2 3 lowpass 500 reverb 25 remix 1p-$PAN1 2p-$PAN2 2 norm -6 fade 0.1 0 0.1
+    sox $f ../output/${file}_destruction.wav trim $SETOFF3 2 overdrive 20 20 overdrive 20 20 overdrive 20 20 norm -18 fade 0.1 0 0.1
+    sox $f ../output/${file}_reverse.wav trim $SETOFF4 6 reverse fade 0.1 0 0.1 norm -6 fade 0.1 0 0.1
 done
 
 echo "... Done!"
