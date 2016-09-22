0d0444e26185d352d22b25d28f44ada2a652f5fa
Thomasklinkenbergmusic <thomasklinkenbergmusic@gmail.com>
2016-09-22 14:16:50 +0200

    Added all sox_try directories to sox_experiments

---
+++ sox_experiments/sox_try2/bashje.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+# Script by Thomas Klinkenberg (2016)
+#-------------------------------------------------------------------------------
+echo
+echo "---Executing--- $0 "
+echo
+#-------------------------------------------------------------------------------
+
+INFILE=$1
+OUTFILE=$2
+LENGHTFLOAT=$(soxi -D $INFILE)
+LENGHT=${LENGHTFLOAT%.*}
+SETOFF1=$(((RANDOM%($LENGHT - 5))+1))
+SETOFF2=$(((RANDOM%($LENGHT - 3))+1))
+SETOFF3=$(((RANDOM%($LENGHT - 2))+1))
+SETOFF4=$(((RANDOM%($LENGHT - 6))+1))
+PAN1=$(((RANDOM%(18))+1))
+PAN2=$(((RANDOM%(18))+1))
+
+# echo "Setoff _file1 = $SETOFF1"
+# echo "Setoff _file2 = $SETOFF2"
+# echo "Setoff _file3 = $SETOFF3"
+# echo "Setoff _file4 = $SETOFF4"
+# echo "Pan1 = $PAN1"
+# echo "Pan2 = $PAN2"
+# echo
+
+# De comments hierboven kunnen alle waardes printen
+
+sox $INFILE ${OUTFILE}_file1.wav trim $SETOFF1 5 norm -6 fade 0.1 0 0.1
+# Maakt een bestand van 5 seconde puur geluid
+
+sox $INFILE ${OUTFILE}_file2.wav trim $SETOFF2 3 lowpass 500 reverb 25 remix 1p-$PAN1 2p-$PAN2 2 norm -6 fade 0.1 0 0.1
+# Maakt een bestand van 3 seconde met een lowpass, reverb en een (random) panning effect
+
+sox $INFILE ${OUTFILE}_file3.wav trim $SETOFF3 2 overdrive 20 20 overdrive 20 20 overdrive 20 20 norm -18 fade 0.1 0 0.1
+# Maakt een bestand van 2 seconde met keiharde overdrive
+
+sox $INFILE ${OUTFILE}_file4.wav trim $SETOFF4 6 reverse fade 0.1 0 0.1 norm -6 fade 0.1 0 0.1
+# Maakt een bestand van 6 seconde die volledig gereversed is
+
+echo "Done!"
+echo "-----"
+echo
+
+#-------------------------------------------------------------------------------
---
+++ sox_experiments/sox_try4/sample_trimmer.sh
@@ -0,0 +1,85 @@
+
+#! /bin/bash
+# Script by Thomas Klinkenberg (2016)
+#-------------------------------------------------------------------------------
+echo
+echo "---------------"
+echo "---Executing--- $0 "
+echo "---------------"
+echo
+#-------------------------------------------------------------------------------
+
+INDIR=$1
+
+if [ $# -lt 1 ];
+  then
+    echo "Error with input directory"
+    echo "No argument!"
+    echo
+    echo "... Exiting program"
+    echo
+    exit
+fi
+
+if [ -d $1 ]
+  then
+    echo "Input directory is $1"
+    echo
+  else
+    echo "Error with input directory."
+    echo "Can't find director $1"
+    echo
+    echo "... Exiting program"
+    echo
+    exit
+  fi
+
+if [ -d output ]
+  then
+    echo "Output directory already exist"
+    echo "Puts converted files in directory output"
+    echo
+  else
+    mkdir -p output
+    echo "Output directory not found!"
+    echo "Making new directory for output"
+    echo
+  fi
+
+echo "Processing files..."
+echo
+
+cp -rf $INDIR ${INDIR}_copy
+cp spaces2_.sh ${INDIR}_copy/spaces2_.sh
+cd ${INDIR}_copy
+./spaces2_.sh
+rm spaces2_.sh
+
+for f in *.mp3;
+  do
+    file=$(basename $f .mp3)
+    sox $f ${file}.wav norm -6
+done
+
+for f in *.wav;
+  do
+    LENGHTFLOAT=$(soxi -D $f)
+    LENGHT=${LENGHTFLOAT%.*}
+    SETOFF1=$(((RANDOM%($LENGHT - 5))+1))
+    SETOFF2=$(((RANDOM%($LENGHT - 3))+1))
+    SETOFF3=$(((RANDOM%($LENGHT - 2))+1))
+    SETOFF4=$(((RANDOM%($LENGHT - 6))+1))
+    PAN1=$(((RANDOM%(18))+1))
+    PAN2=$(((RANDOM%(18))+1))
+    file=$(basename $f .mp3)
+    sox $f ../output/${file}_trim.wav trim $SETOFF1 5 norm -6 fade 0.1 0 0.1
+    sox $f ../output/${file}_otherroom.wav trim $SETOFF2 3 lowpass 500 reverb 25 remix 1p-$PAN1 2p-$PAN2 2 norm -6 fade 0.1 0 0.1
+    sox $f ../output/${file}_destruction.wav trim $SETOFF3 2 overdrive 20 20 overdrive 20 20 overdrive 20 20 norm -18 fade 0.1 0 0.1
+    sox $f ../output/${file}_reverse.wav trim $SETOFF4 6 reverse fade 0.1 0 0.1 norm -6 fade 0.1 0 0.1
+done
+
+cd ..
+rm -rf ${INDIR}_copy
+
+echo "... Done!"
+echo
